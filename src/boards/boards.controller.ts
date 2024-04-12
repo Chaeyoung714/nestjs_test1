ee454fbc880b1e514c59dc6eaff4ce4d3267cc78
Chaeyoung714 <202102760@hufs.ac.kr>
2024-04-12 14:39:36 +0900

    board 기본 crud

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,51 @@
-import { Controller } from '@nestjs/common';
-import {BoardsService} from './boards.service';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
-    constructor(private boardsService: BoardsService) {}
+    constructor(private boardsService: BoardsService) { } //di
+
+    //클라이언트에서 보낸 요청 받음
+    @Get('/') //get 요청이 오면
+    getAllBoard(): Board[] { //다음 메서드를 실행해라  //리턴값 타입 설정
+        return this.boardsService.getAllBoards();
+    }
+    //get -> response: []
+
+    @Post('/')
+    createBoard(
+        // @Body('title') title: string,  //클라 -> 핸들러로
+        // @Body('description') description: string
+        //번거로워 -> DTO로!
+
+        @Body() createBoardDto: CreateBoardDto //dto 사용
+
+    ): Board { //리턴값 = Board객체 하나
+        // return this.boardsService.createBoard(title, description); //핸들러 -> 서비스단으로
+        
+        return this.boardsService.createBoard(createBoardDto);
+    }
+
+
+    // localhost:5000?id=djfafi
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardsService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void {
+        this.boardsService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id: string,
+        @Body('status') status: BoardStatus,
+    ) {
+        return this.boardsService.updateBoardStatus(id, status);
+    }
+    
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+
+import { v1 as uuid } from 'uuid'; //v1-> uuid의 version1 사용한다는 뜻
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    //db를 안 써서 배열 사용..
+    private boards: Board[] = []; //다른 컴포넌트에서 배열 값 수정할 수 없게.
+    //Board 모델 객체를 담을 것
+
+
+    //getboardlist
+    getAllBoards(): Board[] { //boardcontroller에서 사용 / board[] -> 리턴값 지정!
+        return this.boards;
+    }
+
+
+    //createboard
+    // createBoard(title: string, description: string) {
+    createBoard(createBoardDto: CreateBoardDto) {
+        // const title = createBoardDto.title;
+        const {title, description} = createBoardDto;
+        //윗 두줄 => dto 적용
+
+        //1. serializer
+        const board: Board = { 
+            // title: title,
+            // description: description, //이렇게 왼오 같으면 아래와 같이 축약 가능.
+            id: uuid(), //unique 한 값 부여
+            title,
+            description,
+            status: BoardStatus.PUBLIC, //default값 넣음
+        }
+
+        this.boards.push(board);
+        return board;
+    }
+
+
+
+    //getboard
+    getBoardById(id: string): Board {
+        //find -> 배열에서 id에 맞는 board 탐색
+        return this.boards.find((board) => board.id === id)
+    }
+
+
+    //deleteBoard
+    deleteBoard(id: string): void {
+        //삭제니까 return 안해도 됨 (= 반환값도 void)
+        //filter를 사용해서 삭제 조건에 맞는 건 필터링에 걸리게 함.
+        this.boards = this.boards.filter((board) => board.id !== id);
+    }
+
+
+    //updateBoardStatus
+    updateBoardStatus(id: string, status: BoardStatus): Board {
+        const board = this.getBoardById(id);
+        board.status = status;
+        return board;
+    }
+}
